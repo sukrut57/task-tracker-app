fae145e167c370c91dd407cf4e4a7234683a5519
Sukrut K Pasumarthi <kesarisukrut@gmail.com>
2025-03-10 14:53:53 -0400

    task app v1 completed

--- src/main/java/com/backend/shell/commands/TaskComponent.java
+++ src/main/java/com/backend/shell/commands/TaskComponent.java
@@ -1,8 +1,11 @@
 package com.backend.shell.commands;
 
+import com.backend.shell.model.Task;
 import com.backend.shell.service.TaskService;
 import org.springframework.shell.standard.ShellComponent;
 import org.springframework.shell.standard.ShellMethod;
+
+import java.util.List;
 
 @ShellComponent
 public class TaskComponent {
@@ -13,8 +16,100 @@ public class TaskComponent {
         this.taskService = taskService;
     }
 
-    @ShellMethod(key = "addTask", value = "Add a task")
-    public String addTask(String description){
+    /**
+     * Add a task
+     * @param description
+     * Description of the task
+     * @return
+     * Task added successfully
+     */
+    @ShellMethod(key = "add", value = "Add a task")
+    public String addTask(String description) {
         return taskService.createTask(description);
     }
+
+    /**
+     * Update a task
+     * @param id
+     * Id of the task
+     * @param description
+     * Description of the task
+     * @return
+     * Task updated successfully
+     */
+    @ShellMethod(key = "update", value = "Update a task")
+    public String updateTask(long id, String description){
+        return taskService.updateTask(id, description);
+    }
+
+    /**
+     * Mark a task as done
+     * @param id
+     * Id of the task
+     * @return
+     * Task marked as done
+     */
+    @ShellMethod(key ="mark-in-progress", value = "Mark a task in progress")
+    public String markTaskInProgress(long id){
+        return taskService.markTaskInProgress(id);
+    }
+
+    /**
+     * Mark a task as done
+     * @param id
+     * Id of the task
+     * @return
+     * Task marked as done
+     */
+    @ShellMethod(key = "done", value = "Mark a task as done")
+    public String markTaskAsDone(long id){
+        return taskService.markTaskAsDone(id);
+    }
+
+    /**
+     * Delete a task
+     * @param id
+     * Id of the task
+     * @return
+     * Task deleted successfully
+     */
+    @ShellMethod(key = "delete", value = "Delete a task")
+    public String deleteTask(long id){
+        return taskService.deleteTask(id);
+    }
+
+    /**
+     * List all tasks
+     * @return
+     * List of all tasks
+     */
+    @ShellMethod(key="list", value = "list all tasks")
+    public List<String> listTasks(){
+        return taskService.getAllTasks();
+    }
+
+    /**
+     * List all tasks not done
+     * @return
+     * List of all tasks not done
+     */
+
+    @ShellMethod(key = "list todo", value = "List all tasks not done")
+    public List<String> listTasksNotDone() {
+        return taskService.getAllTasksNotDone();
+    }
+
+    @ShellMethod(key = "list in-progress", value = "List all tasks in progress")
+    public List<String> listTasksInProgress() {
+        return taskService.getAllTasksInProgress();
+    }
+
+    @ShellMethod(key = "list done", value = "List all tasks done")
+    public List<String> listTasksDone() {
+        return taskService.getAllTasksDone();
+    }
+
+
+
+
 }
--- src/main/java/com/backend/shell/service/TaskService.java
+++ src/main/java/com/backend/shell/service/TaskService.java
@@ -3,17 +3,97 @@ package com.backend.shell.service;
 import com.backend.shell.model.Task;
 import com.backend.shell.model.TaskStatus;
 import org.springframework.stereotype.Service;
-
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class TaskService {
 
+    private final List<Task> globalTask = new ArrayList<>();
+
+
+    public List<String> getAllTasks(){
+        return printTask(globalTask);
+    }
+
+    public List<String> getAllTasksNotDone(){
+        List<Task> notDoneTasks = new ArrayList<>();
+        if(globalTask.isEmpty()){
+            return null;
+        }
+        globalTask.stream().filter(task ->
+                task.getStatus().equals(TaskStatus.todo)).forEach(notDoneTasks::add);
+        return printTask(notDoneTasks);
+    }
+
+
     public String createTask(String description){
         LocalDateTime currentDate = LocalDateTime.now();
         Task task = new Task(description, TaskStatus.todo, currentDate, currentDate);
-        //save it to a json file
-        return "Task added successfully" + task.getId();
+        //todo save it to a json file
+        globalTask.add(task);
+        return "Task added successfully: " + task.getId();
     }
 
+    public String updateTask(long id, String description){
+        Task retrieveTask = globalTask.stream().filter(task -> task.getId() == id).findFirst().orElse(null);
+        if(retrieveTask == null){
+            return "Task not found";
+        }
+        retrieveTask.setDescription(description);
+        retrieveTask.setUpdatedAt(LocalDateTime.now());
+        return "Task updated successfully";
+    }
+
+    private List<String> printTask(List<Task> tasks){
+        List<String> taskList = new ArrayList<>();
+        tasks.forEach(task -> taskList.add(task.getId() + " " + task.getDescription() + " " + task.getStatus() + " " +  task.getCreatedAt() + " " + task.getUpdatedAt()));
+        return taskList;
+    }
+
+    public String deleteTask(long id) {
+        Task retrieveTask = globalTask.stream().filter(task -> task.getId() == id).findFirst().orElse(null);
+        if(retrieveTask == null){
+            return "Task not found";
+        }
+        return "Task deleted successfully";
+    }
+
+    public String markTaskInProgress(long id) {
+        Task retrieveTask = globalTask.stream().filter(task -> task.getId() == id).findFirst().orElse(null);
+        if(retrieveTask == null){
+            return "Task not found";
+        }
+        retrieveTask.setStatus(TaskStatus.inProgress);
+        retrieveTask.setUpdatedAt(LocalDateTime.now());
+        return "Task marked in progress";
+    }
+
+
+    public String markTaskAsDone(long id) {
+        Task retrieveTask = globalTask.stream().filter(task -> task.getId() == id).findFirst().orElse(null);
+        if(retrieveTask == null){
+            return "Task not found";
+        }
+        retrieveTask.setStatus(TaskStatus.done);
+        retrieveTask.setUpdatedAt(LocalDateTime.now());
+        return "Task marked as done";
+    }
+
+    public List<String> getAllTasksInProgress() {
+        List<Task> taskList = globalTask.stream().filter(task -> task.getStatus().equals(TaskStatus.inProgress)).toList();
+        if(taskList.isEmpty()){
+            return new ArrayList<>();
+        }
+        return printTask(taskList);
+    }
+
+    public List<String> getAllTasksDone() {
+        List<Task> taskList = globalTask.stream().filter(task -> task.getStatus().equals(TaskStatus.done)).toList();
+        if(taskList.isEmpty()){
+            return new ArrayList<>();
+        }
+        return printTask(taskList);
+    }
 }
